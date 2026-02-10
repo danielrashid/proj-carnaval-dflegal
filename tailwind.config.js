516e7e9d64b9025cffdd164a84f85218bb19afc2
danielrashid <danielraraujo2@gmail.com>
2026-02-10 20:50:21 -0300

    Primeiro commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'df-blue': '#1e3a8a',
+      }
+    },
+  },
+  plugins: [],
+}
